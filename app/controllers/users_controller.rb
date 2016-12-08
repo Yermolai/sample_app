b5fb2b851aec684eeb538e4d200dd3b4b504e9ef
Yermolai <ermolaiy@gmail.com>
2016-12-08 23:22:57 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    ###### final?
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+#     flash[:info] = "Welcome to the Sample App!"
+#     flash[:warning] = "Welcome to the Sample App!"
+#     flash[:danger] = "Welcome to the Sample App!"
+      redirect_to @user # Handle a successful save. Equals:     redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:  "",
+                                  email: "user@invalid",
+                                  password:              "foo",
+                                  password_confirmation: "bar" } }
+    end
+
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+    #assert_select ’form[action="/signup"]’
+  end
+
+
+
+  test "successful signup" do
+    get signup_path
+    assert_difference 'User.count',1 do
+       post signup_path, params: { user: { name:  "James",
+                                           email: "user@valid.com",
+                                           password:              "123456",
+                                           password_confirmation: "123456" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+
+    #assert_select 'div.alert'
+    #assert_select 'div.alert-success'
+    assert_not flash.empty?
+    assert_equal flash[:success], "Welcome to the Sample App!"
+  end
+
+end
